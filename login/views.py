ee962cb278d2532bcf9d7e93ea5529d8851907c2
gmacchi93 <guillemacchi01@gmail.com>
2016-04-08 15:28:02 -0400

    -se agrego una correcion

--- login/views.py
+++ login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 
+
 def login_user(request):
     state = "Please log in below..."
     if request.user.is_authenticated():
