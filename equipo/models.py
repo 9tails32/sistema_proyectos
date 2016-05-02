6aaa44e07d9856e6bd0164dc0e67d8627adca6dd
Ruben <shishi9tail@hotmail.com>
2016-05-02 09:20:07 -0400

    -Añadido campo Usuarios a equipo

--- equipo/models.py
+++ equipo/models.py
@@ -5,9 +5,15 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import Group
 from django.db import models
 from proyecto.models import Proyecto
-
+from login.models import Usuario
 # Create your models here.
 
 
 class Equipo(Group):
+    """
+    Clase Equipo que heredea del modelo Group de django.
+    proyecto = proyecto al que pertenece el equipo.
+    usuarios = usuarios miembros del equipo.
+    """
     proyecto = models.ForeignKey(Proyecto,null=True)
+    usuarios = models.ManyToManyField(Usuario)
