fa6eb4e77e05f620001d9ab0f3e4e8e2fae01948
gmacchi93 <guillemacchi01@gmail.com>
2016-05-02 00:15:00 -0400

    arreglando bardo de migrations

--- login/admin.py
+++ login/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from .models import Usuario
-class UsuarioAdmin(admin.ModelAdmin):
-    list_display = ('username','first_name','last_name','email','is_active')
+
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
+
+class MyUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = Usuario
+
+class MyUserAdmin(UserAdmin):
+    form = MyUserChangeForm
+
+    fieldsets = UserAdmin.fieldsets + (
+            (None, {'fields': ('cedula','direccion','formato_notificaciones','hora_notificaciones','noti_creacion_proyecto','noti_creacion_usuario','noti_creacion_equipos')}),
+    )
 
 # Register your models here.
-admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(Usuario, MyUserAdmin)
