8d9368e7b056bf2f74bb9875a464682e6aeb56ce
rauleb <rauleburro@gmail.com>
2016-05-15 17:14:30 -0400

    -agregamos viewspara US

--- US/admin.py
+++ US/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from US.models import *
 # Register your models here.
 admin.site.register(TipoUS)
+admin.site.register(Actividades)
+admin.site.register(US)
--- US/views.py
+++ US/views.py
@@ -4,13 +4,13 @@ from django.http import HttpResponseRedirect
 from django.forms import formset_factory, BaseFormSet
 from US.models import *
 from US.forms import *
+
+
 # Create your views here.
 
 @login_required(None, 'login', '/login/')
-@permission_required('US.crear_tipo_US',raise_exception=True)
+@permission_required('US.crear_tipo_US', raise_exception=True)
 def create_tipo(request):
-
-
     if request.method == 'POST':
         form = TipoUSForm(request.POST)
         if form.is_valid():
@@ -22,10 +22,11 @@ def create_tipo(request):
     else:
         form = TipoUSForm()
 
-    return render(request, 'tipo_us_create.html', {'form': form},)
+    return render(request, 'tipo_us_create.html', {'form': form}, )
+
 
 @login_required(None, 'login', '/login/')
-@permission_required('US.crear_actividades',raise_exception=True)
+@permission_required('US.crear_actividades', raise_exception=True)
 def create_actividad(request, pk):
     try:
         tipo = TipoUS.objects.get(pk=pk)
@@ -40,17 +41,49 @@ def create_actividad(request, pk):
             actividad.nombre = form.cleaned_data['nombre']
             actividad.save()
 
-            return HttpResponseRedirect('/us/actividades/'+str(tipo.id))
+            return HttpResponseRedirect('/us/actividades/' + str(tipo.id))
     else:
         form = ActividadesForm()
 
-    return render(request,'actividad_create.html', {'form': form})
+    return render(request, 'actividad_create.html', {'form': form})
+
 
 def list_actividades(request, pk):
     try:
-        tipo=TipoUS.objects.get(pk=pk)
+        tipo = TipoUS.objects.get(pk=pk)
     except:
         return HttpResponseRedirect('/us/tipo/create/')
     actividades_list = Actividades.objects.filter(tipoUS__pk=pk)
 
-    return render(request, 'actividades_list.html', {'actividades_list': actividades_list,'object': tipo})
+    return render(request, 'actividades_list.html', {'actividades_list': actividades_list, 'object': tipo})
+
+
+@login_required(None, 'login', '/login/')
+@permission_required('')
+def create_us(request, pk):
+    try:
+        proyecto = Proyecto.objects.get(pk=pk)
+    except:
+        print 'Error'
+
+    if request.method == 'POST':
+        form = USForm(request.POST)
+        if form.is_valid():
+            us = US()
+            us.proyecto = proyecto
+            us.descripcion_corta = form.cleaned_data['descripcion_corta']
+            us.descripcion_larga = form.cleaned_data['descripcion_larga']
+            us.tiempo_planificado = form.cleaned_data['tiempo_planificado']
+            us.valor_negocio = form.cleaned_data['valor_negocio']
+            us.urgencia = form.cleaned_data['urgencia']
+            us.usuario_asignado = form.cleaned_data['usuario_asignado']
+            us.tipoUS = form.cleaned_data['tipoUS']
+            us.actividad = form.cleaned_data['actividad']
+            us.estado_actividad = form.cleaned_data['estado_actividad']
+            us.save()
+
+            return HttpResponseRedirect('/us/us/' + str(us.id))
+        else:
+            form = TipoUSForm()
+
+        return render(request, 'us_create.html', {'form': form}, )
