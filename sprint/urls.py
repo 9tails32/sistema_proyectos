839a05e72344abc603b964d8ae448b6347057664
gmacchi93 <guillemacchi01@gmail.com>
2016-05-15 20:11:39 -0400

    - se agrego asignar us a sprint

--- sprint/urls.py
+++ sprint/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     url(r'^create/(?P<pk>[0-9]+)/$',create_sprint),
+    url(r'^asignar_us/(?P<pk>[0-9]+)/$', asignar_us),
     url(r'^(?P<pk>[0-9]+)/$',detail_sprint,name='sprint_detail')
 ]
--- sprint/views.py
+++ sprint/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, permission_required
 from django.http import HttpResponseRedirect
 from sprint.models import Sprint
-from sprint.forms import SprintForm
+from sprint.forms import *
 from proyecto.models import Proyecto
 
 
@@ -41,3 +41,42 @@ def detail_sprint(request,pk):
         return HttpResponseRedirect('/proyecto/')
 
     return render(request, 'sprint_detail.html', {'sprint': sprint})
+
+@login_required(None, 'login', '/login/')
+@permission_required('proyecto.can_cambiar_estado', raise_exception=True)
+def asignar_us(request, pk):
+    """
+        Funcion para actualizar el estado del proyecto utilizando el form CambiarEstadoForm.
+        Recibe en el request el form completado, o displaya uno con los datos previos del proyecto en
+        caso de que no se llame a post. Controla la validez del form antes de guardarlo como un proyecto
+         nuevo en la base de datos.
+        Parametros: Recibe el request y el pk del proyecto a editar.
+        Retorna:
+        -El render del template proyecto_create.html en caso de form vacio o invalido.
+        -Redireccion a lista de proyectos si el form es valido
+
+    """
+    try:
+        sprint = Sprint.objects.get(pk=pk)
+    except:
+        return HttpResponseRedirect('/proyecto/')
+
+    uss = US.objects.filter(Q(proyecto=sprint.proyecto,sprint=None)|Q(sprint=sprint))
+
+    if request.method == 'POST':
+        form = AsignarUSForm(request.POST)
+        form.fields["uss"].queryset = uss
+        if form.is_valid():
+            sprint.uss = form.cleaned_data['uss']
+            acumulador = 0
+            for us in sprint.uss.all():
+                acumulador += us.tiempo_planificado
+            sprint.duracion = acumulador
+            sprint.save()
+
+            return HttpResponseRedirect('/sprint/' + str(sprint.id))
+    else:
+        form = AsignarUSForm(initial={'uss':sprint.uss.all})
+        form.fields["uss"].queryset = uss
+
+    return render(request, 'asignar_us.html', {'form': form, 'sprint': sprint})
