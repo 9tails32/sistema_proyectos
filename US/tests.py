568cb2909539b040a43423fa0fcd57da5dd1d44a
ruben <shishi9tail@hotmail.com>
2016-05-16 00:14:58 -0400

    -Creacion de tests de sprints, tipos de us, actividades  y us

--- US/tests.py
+++ US/tests.py
@@ -1,3 +1,107 @@
 from django.test import TestCase
-
+from proyecto.models import Proyecto
+from login.models import Usuario
+from cliente.models import Cliente
+from US.forms import *
+from django.utils import timezone
+from US.models import *
+from equipo.models import Equipo
 # Create your tests here.
+class TestViewUS(TestCase):
+    def setUp(self):
+        usuario = Usuario.objects.create_user(username='user1', email='asd@asd.com',password='mangekyou', is_staff=True,is_superuser=True)
+        cliente = Cliente(nombre='client1', direccion='Azara', email='asd@hotmail.com')
+        cliente.save()
+        usuario.save()
+        self.client.login(username='user1',password='mangekyou')
+        proyecto=Proyecto(id=1,nombre='proyecto',
+                             fecha_fin=timezone.now(), fecha_inicio=timezone.now(),
+                             lider_proyecto=usuario, cliente=cliente,
+                             descripcion= 'Descripcion',
+                             )
+        proyecto.save()
+        equipo = Equipo(proyecto=proyecto, nombre='equipo1')
+        equipo.save()
+        equipo.permisos={'1'}
+        equipo.usuarios={'1'}
+        equipo.save()
+
+        tipo = TipoUS(pk=1, nombre='tipo')
+        tipo.save()
+        actividad = Actividades(pk=3, nombre='actividad', tipoUS=TipoUS.objects.get(pk=1))
+        actividad.save()
+        us = US(descripcion_corta='a', descripcion_larga='a', tiempo_planificado=3, valor_negocio=1,
+                urgencia=1, tipoUS=tipo, usuario_asignado=Usuario.objects.get(pk=1))
+
+        us.save()
+
+
+
+    def test_create_tipo_template(self):
+
+        resp = self.client.get('/us/tipo/create/')
+        self.assertEqual(resp.status_code, 200)
+
+
+    def test_create_tipo_form_valid(self):
+
+        form=TipoUSForm({'nombre':'sprint 1'})
+        self.assertTrue(form.is_valid())
+
+    def test_detail_tipo(self):
+        resp = self.client.get('/us/tipo/1/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_update_tipo_template(self):
+        resp = self.client.get('/us/tipo/update/1/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_list_tipo(self):
+        resp = self.client.get('/us/tipo/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_create_actividades_template(self):
+        resp = self.client.get('/us/actividades/create/1/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_create_actividades_form_valid(self):
+        form = ActividadesForm({'nombre': 'sprint 1'})
+        self.assertTrue(form.is_valid())
+
+    def test_delete_actividad_template(self):
+
+
+        resp = self.client.get('/us/actividades/delete/3/')
+        self.assertEqual(resp.status_code, 302)
+
+    def test_list_actividades(self):
+        resp = self.client.get('/us/actividades/1/')
+        self.assertEqual(resp.status_code, 200)
+
+
+
+    def test_create_us_form_valid(self):
+        tipo = TipoUS(nombre='tipo')
+        tipo.save()
+        usuario = Usuario(username='asd',password='asdadsadasda')
+        usuario.save()
+        form = USForm({'descripcion_corta':'a', 'descripcion_larga':'a', 'tiempo_planificado':'3', 'valor_negocio':'1',
+                'urgencia':'1', 'tipoUS': str(tipo.pk), 'usuario_asignado':str(usuario.pk)})
+        self.assertFalse(form.is_valid())
+
+
+
+
+
+    def test_cambiar_actividad_us_template(self):
+        resp = self.client.get('/us/us/cambiar_actividad/1/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_delete_estado_actividad_us_template(self):
+        resp = self.client.get('/us/us/cambiar_estado_actividad/1/')
+        self.assertEqual(resp.status_code, 200)
+
+
+
+
+
--- sprint/tests.py
+++ sprint/tests.py
@@ -1,3 +1,64 @@
 from django.test import TestCase
+from proyecto.models import Proyecto
+from login.models import Usuario
+from cliente.models import Cliente
+from sprint.models import Sprint
+from sprint.forms import SprintForm, AsignarUSForm
+from django.utils import timezone
+from US.models import *
 
 # Create your tests here.
+class TestViewSprint(TestCase):
+    def setUp(self):
+        usuario = Usuario.objects.create_user(username='user1', email='asd@asd.com',password='mangekyou', is_staff=True,is_superuser=True)
+        cliente = Cliente(nombre='client1', direccion='Azara', email='asd@hotmail.com')
+        cliente.save()
+        usuario.save()
+        self.client.login(username='user1',password='mangekyou')
+        proyecto=Proyecto(id=1,nombre='proyecto',
+                             fecha_fin=timezone.now(), fecha_inicio=timezone.now(),
+                             lider_proyecto=usuario, cliente=cliente,
+                             descripcion= 'Descripcion',
+                             )
+        proyecto.save()
+
+
+
+
+
+    def test_create_template(self):
+
+        resp = self.client.get('/sprint/create/1/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_create_form_valid(self):
+
+        form=SprintForm({'nombre':'sprint 1'})
+        self.assertTrue(form.is_valid())
+        resp = self.client.post('/sprint/create/1/',{'form':form})
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue(form.is_valid())
+
+
+    def test_create_no_proyect(self):
+
+        resp = self.client.post('/sprint/create/2/')
+        self.assertEqual(resp.status_code, 302)
+
+    def test_detail(self):
+        resp = self.client.post('/sprint/1/')
+        self.assertEqual(resp.status_code, 302)
+
+    def test_asignarUS(self):
+        sprint = Sprint(id=3, nombre='sprint 3', proyecto=Proyecto.objects.get(pk=1))
+        sprint.save()
+        tipo = TipoUS(nombre='tipo')
+        tipo.save()
+        us = US(descripcion_corta='a', descripcion_larga='a', tiempo_planificado=3, valor_negocio=1,
+                urgencia=1, tipoUS=tipo, usuario_asignado=Usuario.objects.get(pk=1))
+        us.save()
+        sprint.duracion = 3
+        sprint.save()
+        resp = self.client.post('/sprint/asignar_us/3')
+        self.assertEqual(resp.status_code, 301)
+
